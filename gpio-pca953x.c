09340cfe0fd5022b6121dcc0edc832bb71449d67
Issam Sayyaf <92667668+IssamSayyaf@users.noreply.github.com>
2025-04-07 22:30:57 +0200

    Update PCA953x GPIO driver with full content - Part 2

--- gpio-pca953x.c
+++ gpio-pca953x.c
@@ -158,3 +158,143 @@ static void pca953x_gpio_set_value(struct gpio_chip *gc, unsigned int off, int v
 exit:
         mutex_unlock(&chip->i2c_lock);
 }
+
+
+static int pca953x_read_regs(struct pca953x_chip *chip, int reg, u8 *val)
+{
+        int ret;
+
+        if (chip->gpio_chip.ngpio <= 8) {
+                ret = i2c_smbus_read_byte_data(chip->client, reg);
+                *val = ret;
+        } else if (chip->gpio_chip.ngpio >= 24) {
+                int bank_shift = fls((chip->gpio_chip.ngpio - 1) / BANK_SZ);
+
+                ret = i2c_smbus_read_i2c_block_data(chip->client,
+                                (reg << bank_shift) | REG_ADDR_AI,
+                                NBANK(chip), val);
+        } else {
+                ret = i2c_smbus_read_word_data(chip->client, reg << 1);
+                val[0] = (u16)ret & 0xFF;
+                val[1] = (u16)ret >> 8;
+        }
+        if (ret < 0) {
+                dev_err(&chip->client->dev, "failed reading register\n");
+                return ret;
+        }
+
+        return 0;
+}
+
+static int pca953x_write_regs(struct pca953x_chip *chip, int reg, u8 *val)
+{
+        int ret = 0;
+
+        if (chip->gpio_chip.ngpio <= 8)
+                ret = i2c_smbus_write_byte_data(chip->client, reg, *val);
+        else if (chip->gpio_chip.ngpio >= 24) {
+                int bank_shift = fls((chip->gpio_chip.ngpio - 1) / BANK_SZ);
+                ret = i2c_smbus_write_i2c_block_data(chip->client,
+                                (reg << bank_shift) | REG_ADDR_AI,
+                                NBANK(chip), val);
+        } else {
+                switch (chip->chip_type) {
+                        case PCA953X_TYPE:
+                                ret = i2c_smbus_write_word_data(chip->client,
+                                                reg << 1, (u16) *val);
+                                break;
+                        case PCA957X_TYPE:
+                                ret = i2c_smbus_write_byte_data(chip->client, reg << 1,
+                                                val[0]);
+                                if (ret < 0)
+                                        break;
+                                ret = i2c_smbus_write_byte_data(chip->client,
+                                                (reg << 1) + 1,
+                                                val[1]);
+                                break;
+                }
+        }
+
+        if (ret < 0) {
+                dev_err(&chip->client->dev, "failed writing register\n");
+                return ret;
+        }
+
+        return 0;
+}
+
+static ssize_t Direction_show(struct kobject *kobj, struct kobj_attribute *attr,char *buf)
+{
+        return sprintf(buf, "%d\n", Direction);
+}
+
+/*
+ *This function will take the direction value and store it to the Direction register of IOMUX.
+ *This Direction Setting will be applied to all Ports(0-4) of the IOMUX.
+ */
+static ssize_t Direction_store(struct kobject *kobj, struct kobj_attribute *attr,char *buf, size_t count)
+{
+        u8 val[MAX_BANK];
+        u8 regis_output[MAX_BANK] = {0};
+        int ret = -1;
+        sscanf(buf, "%du", &Direction);
+        memset(val, Direction, NBANK(my_chip));
+
+
+        ret = pca953x_write_regs(my_chip, PCA953X_DIRECTION, val);
+
+        ret = pca953x_read_regs(my_chip, PCA953X_DIRECTION, regis_output);
+        //printk("Direction register readings:%d,%d,%d,%d,%d\n",regis_output[0],regis_output[1],regis_output[2],regis_output[3],regis_output[4]);
+        return count;
+}
+
+
+static ssize_t Output_show(struct kobject *kobj, struct kobj_attribute *attr,char *buf)
+{
+        return sprintf(buf, "%d\n", Output);
+}
+
+/*
+ *This function will set(1) the Output pin given in the argument.
+ */
+static ssize_t Output_store(struct kobject *kobj, struct kobj_attribute *attr,char *buf, size_t count)
+{
+        u8 val[MAX_BANK];
+        u8 regis_output[MAX_BANK] = {0};
+        int ret = -1;
+        sscanf(buf, "%du", &Output);
+        memset(val, Output, NBANK(my_chip));
+
+        pca953x_gpio_set_value(&my_chip->gpio_chip, Output,1);
+
+	ret = pca953x_read_regs(my_chip, PCA953X_OUTPUT, regis_output);
+        //printk("Output Config register readings:%d,%d,%d,%d,%d\n",regis_output[0],regis_output[1],regis_output[2],regis_output[3],regis_output[4]);
+        return count;
+}
+
+static ssize_t Invert_Output_show(struct kobject *kobj, struct kobj_attribute *attr,char *buf)
+{
+        return sprintf(buf, "%d\n", Invert_Output);
+}
+
+/*This function will reset(0) the Output pin given in the argument.
+ *
+ */
+static ssize_t Invert_Output_store(struct kobject *kobj, struct kobj_attribute *attr,char *buf, size_t count)
+{
+        u8 val[MAX_BANK];
+        u8 regis_output[MAX_BANK] = {0};
+        int ret = -1;
+        sscanf(buf, "%du", &Invert_Output);
+        memset(val, Invert_Output, NBANK(my_chip));
+
+        pca953x_gpio_set_value(&my_chip->gpio_chip, Invert_Output,0);
+
+        ret = pca953x_read_regs(my_chip, PCA953X_OUTPUT, regis_output);
+        //printk("Output Config register readings:%d,%d,%d,%d,%d\n",regis_output[0],regis_output[1],regis_output[2],regis_output[3],regis_output[4]);
+        return count;
+}
+
+static struct kobj_attribute Direction_attribute =__ATTR(Direction, 0660, Direction_show,Direction_store);
+static struct kobj_attribute Output_attribute =__ATTR(Output, 0660, Output_show,Output_store);
+static struct kobj_attribute Invert_Output_attribute =__ATTR(Invert_Output, 0660, Invert_Output_show, Invert_Output_store);
